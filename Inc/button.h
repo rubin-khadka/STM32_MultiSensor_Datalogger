6729f09ff9d6c8f7c63c954fea371c7a93b9fa04
rubin-khadka <rubin.robotic@gmail.com>
2026-02-27 10:02:12 +0100

    added two more button to save read and erase data from the memory

--- Inc/button.h
+++ Inc/button.h
@@ -21,6 +21,8 @@ void Button_Init(void);
 DisplayMode_t Button_GetMode(void);
 void Button_NextMode(void);
 void EXTI0_IRQHandler(void);
+void EXTI1_IRQHandler(void);
+void EXTI2_IRQHandler(void);
 void TIMER4_Init(void);
 void TIM4_IRQHandler(void);
 
